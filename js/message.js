5cecf5780b297b3d547229ebcf2cd580e36875d6
Anasisa <120271781+Anasisa@users.noreply.github.com>
2024-12-29 00:48:09 +0100

    batch40 website
    Hot batch40 website project

---
+++ js/message.js
@@ -0,0 +1,98 @@
+function msg(){
+alert("You Look Gorgeous ");
+}
+{
+	let msg
+msg=prompt("I Love you!!")
+console.log(msg)
+}
+
+//val1=10
+//val2=20
+//console.log(val1)
+//console.log(val2)
+
+//let val3="Hello Dear"
+//val3="Hello Dear"
+//{
+	//alert("Hello")
+	//prompt("Enter Your Email", "type your email here")
+	// This result on the console
+	//let msg
+	//msg=prompt("Enter Your Email", "type your email here")
+	//console.log(msg)
+	
+	//This display result on the web page
+	
+	//let dsp = document.getElementById('display');
+	
+	//dsp.innerHTML = msg
+	//confirm ("Are you sure you want to exit??")
+			//confirm message
+	//let res = confirm ("Are you sure you want to exit??")
+	//console.log(res)
+	
+	//JS OPERATORS display both on console and html page
+	let v1=100
+	v2=45
+	/*console.log(v1*v2)
+	console.log(v1+v2)
+	console.log(v1-v2)
+	document.write(--v1)
+	res= v1 % v2
+	console.log(v1," %", v2," = ",res)
+	*/
+	
+	res= v1==v2
+	console.log(res)
+	
+	res= v1>v2
+	console.log(res)
+	
+	res= v1>v2 || v1==v2
+	console.log(res)
+	
+	res= v1>v2 && v1==v2
+	console.log(res)
+	
+	res= v1>=v2
+	console.log(res)
+	
+	v1-=5
+	console.log(v1)
+	
+	v1+=5
+	console.log(v1)
+	
+	v1*=5
+	console.log(v1)
+	
+	v1/=5
+	console.log(v1)
+	
+	v1%=5
+	console.log(v1)
+	
+	//Ternary operation
+	
+	v3=10
+	v4=55
+	v3>v4 ? alert("less than"): alert("greater than")
+	
+	//JS string and Total Number of string
+	text="Hello dear hope u are doing great"
+	text1="I am Fine"
+	console.log("Total No. of String is",text.length)
+	console.log(text1)
+	
+	//let hour=5
+//if (hour<6 || hour >9){
+	//alert("The office is closed.");
+//}
+//let hours=12
+//let isWeekend=true;
+//if(hours <10 || hours >18 || isWeekend ){
+	//alert("The Office is close.");|| isWeekend;
+//}
+	
+
